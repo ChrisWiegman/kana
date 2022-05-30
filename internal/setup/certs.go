4e5ef18663780704734c97b9d361f3d516d2f808
Chris Wiegman <chris@chriswiegman.com>
2022-05-30 10:56:38 -0400

    Cleanup CA Generation

--- internal/setup/certs.go
+++ internal/setup/certs.go
@@ -14,45 +14,55 @@ var caKey = "certs/kana.ca.key"
 
 func GenerateCA() {
 
-	caCertPath, err := config.GetConfigRoot()
+	fmt.Println("Checking for Root CA...")
+
+	appConfigPath, err := config.GetConfigRoot()
 	if err != nil {
 		panic(err)
 	}
 
-	caCertFile := path.Join(caCertPath, caCert)
-	caKeyFile := path.Join(caCertPath, caKey)
+	certDir := path.Join(appConfigPath, "certs")
+	caCertFile := path.Join(appConfigPath, caCert)
+	caKeyFile := path.Join(appConfigPath, caKey)
 
 	_, err = os.Stat(caKeyFile)
 	if err != nil && !os.IsNotExist(err) {
 		fmt.Println(err)
 	}
 
-	err = exec.Command(
-		"openssl",
-		"genrsa",
-		"-out",
-		caKeyFile,
-		"4096").Run()
-	if err != nil {
-		fmt.Println(err)
-	}
+	if os.IsNotExist(err) {
 
-	err = exec.Command(
-		"openssl",
-		"req",
-		"-x509",
-		"-new",
-		"-nodes",
-		"-key",
-		caKeyFile,
-		"-sha256",
-		"-days",
-		"7300",
-		"-out",
-		caCertFile,
-		"-subj",
-		"/C=US/ST=Florida/L=Sarasota/O=Kana/OU=Development/CN=Kana Development CA").Run()
-	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Root CA not found. Generating Root CA...")
+
+		os.MkdirAll(certDir, 0700)
+
+		err = exec.Command(
+			"openssl",
+			"genrsa",
+			"-out",
+			caKeyFile,
+			"4096").Run()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		err = exec.Command(
+			"openssl",
+			"req",
+			"-x509",
+			"-new",
+			"-nodes",
+			"-key",
+			caKeyFile,
+			"-sha256",
+			"-days",
+			"7300",
+			"-out",
+			caCertFile,
+			"-subj",
+			"/C=US/ST=Florida/L=Sarasota/O=Kana/OU=Development/CN=Kana Development CA").Run()
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
