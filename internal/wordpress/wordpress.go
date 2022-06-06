a02bfd60fd81c0c388ff16062193e25363a4aac3
Chris Wiegman <chris@chriswiegman.com>
2022-06-05 20:18:05 -0400

    Working on wp-cli and persistent data

--- internal/wordpress/wordpress.go
+++ internal/wordpress/wordpress.go
@@ -2,9 +2,12 @@ package wordpress
 
 import (
 	"fmt"
+	"os"
+	"path"
 
 	"github.com/ChrisWiegman/kana/internal/docker"
 	"github.com/ChrisWiegman/kana/internal/traefik"
+	"github.com/docker/docker/api/types/mount"
 )
 
 func StopWordPress(controller *docker.Controller) error {
@@ -32,6 +35,18 @@ func StartWordPress(controller *docker.Controller) error {
 		return err
 	}
 
+	siteDir := path.Join(controller.Config.ConfigRoot, "sites", controller.Config.CurrentDirectory)
+	appDir := path.Join(siteDir, "app")
+	databaseDir := path.Join(siteDir, "database")
+
+	if err := os.MkdirAll(appDir, 0750); err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(databaseDir, 0750); err != nil {
+		return err
+	}
+
 	wordPressContainers := []docker.ContainerConfig{
 		{
 			Name:        fmt.Sprintf("kana_%s_database", controller.Config.CurrentDirectory),
@@ -46,6 +61,13 @@ func StartWordPress(controller *docker.Controller) error {
 			},
 			Labels: map[string]string{
 				"kana.site": controller.Config.CurrentDirectory,
+			},
+			Volumes: []mount.Mount{
+				{
+					Type:   mount.TypeBind,
+					Source: databaseDir,
+					Target: "/var/lib/mysql",
+				},
 			},
 		},
 		{
@@ -67,6 +89,35 @@ func StartWordPress(controller *docker.Controller) error {
 				fmt.Sprintf("traefik.http.routers.wordpress-%s.rule", controller.Config.CurrentDirectory):             fmt.Sprintf("Host(`%s.%s`)", controller.Config.CurrentDirectory, controller.Config.SiteDomain),
 				fmt.Sprintf("traefik.http.routers.wordpress-%s.tls", controller.Config.CurrentDirectory):              "true",
 				"kana.site": controller.Config.CurrentDirectory,
+			},
+			Volumes: []mount.Mount{
+				{
+					Type:   mount.TypeBind,
+					Source: appDir,
+					Target: "/var/www/html",
+				},
+			},
+		},
+		{
+			Name:        fmt.Sprintf("kana_%s_wordpress_cli", controller.Config.CurrentDirectory),
+			Image:       "wordpress:cli",
+			NetworkName: "kana",
+			HostName:    fmt.Sprintf("kana_%s_wordpress_cli", controller.Config.CurrentDirectory),
+			Env: []string{
+				fmt.Sprintf("WORDPRESS_DB_HOST=kana_%s_database", controller.Config.CurrentDirectory),
+				"WORDPRESS_DB_USER=wordpress",
+				"WORDPRESS_DB_PASSWORD=wordpress",
+				"WORDPRESS_DB_NAME=wordpress",
+			},
+			Labels: map[string]string{
+				"kana.site": controller.Config.CurrentDirectory,
+			},
+			Volumes: []mount.Mount{
+				{
+					Type:   mount.TypeBind,
+					Source: siteDir,
+					Target: "/var/www/html",
+				},
 			},
 		},
 	}
