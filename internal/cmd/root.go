b11a76017978b04f0fe82bcf8b92327282a9fcfc
Chris Wiegman <chris@chriswiegman.com>
2022-07-03 17:06:47 -0400

    Root doesn't need to run anything

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -20,9 +20,6 @@ func newRootCommand() (*cobra.Command, config.AppConfig) {
 	cmd := &cobra.Command{
 		Use:   "kana",
 		Short: "Kana is a simple WordPress development tool designed for plugin and theme developers.",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("")
-		},
 	}
 
 	return cmd, appConfig
