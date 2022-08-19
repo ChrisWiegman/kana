552ff3c15dfc988b3847c88288682db89d41509b
Chris Wiegman <chris@chriswiegman.com>
2022-08-19 11:38:58 -0400

    Add shorthand aliases for start command

--- internal/cmd/start.go
+++ internal/cmd/start.go
@@ -27,10 +27,10 @@ func newStartCommand(site *site.Site) *cobra.Command {
 		Args: cobra.NoArgs,
 	}
 
-	cmd.Flags().BoolVar(&flagXdebug, "xdebug", false, "Enable Xdebug when starting the container.")
-	cmd.Flags().BoolVar(&flagIsPlugin, "plugin", false, "Run the site as a plugin using the current folder as the plugin source.")
-	cmd.Flags().BoolVar(&flagIsTheme, "theme", false, "Run the site as a theme using the current folder as the theme source.")
-	cmd.Flags().BoolVar(&flagLocal, "local", false, "Installs the WordPress files in your current path at ./wordpress instead of the global app path.")
+	cmd.Flags().BoolVarP(&flagXdebug, "xdebug", "x", false, "Enable Xdebug when starting the container.")
+	cmd.Flags().BoolVarP(&flagIsPlugin, "plugin", "p", false, "Run the site as a plugin using the current folder as the plugin source.")
+	cmd.Flags().BoolVarP(&flagIsTheme, "theme", "t", false, "Run the site as a theme using the current folder as the theme source.")
+	cmd.Flags().BoolVarP(&flagLocal, "local", "l", false, "Installs the WordPress files in your current path at ./wordpress instead of the global app path.")
 
 	return cmd
 
