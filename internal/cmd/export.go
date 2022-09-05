84cf8b09568203f1fc9773efc66497b1cd5cbd8d
Chris Wiegman <chris@chriswiegman.com>
2022-09-05 14:43:00 -0400

    Don't run export if site isn't running

--- internal/cmd/export.go
+++ internal/cmd/export.go
@@ -27,6 +27,11 @@ func newExportCommand(site *site.Site) *cobra.Command {
 
 func runExport(cmd *cobra.Command, args []string, site *site.Site) {
 
+	if !site.IsSiteRunning() {
+		fmt.Println("The export command only works on a running site.  Please run 'kana start' to start the site.")
+		os.Exit(1)
+	}
+
 	err := site.ExportSiteSettings()
 	if err != nil {
 		fmt.Println(err)
