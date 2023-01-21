1cdfacbc757855740f99663c646342d9c09d256d
Chris Wiegman <chris@chriswiegman.com>
2023-01-21 17:43:52 -0500

    Simplify progress loading when pulling images

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-playground/validator/v10 v10.11.1
 	github.com/logrusorgru/aurora/v4 v4.0.0
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/moby/moby v20.10.23+incompatible
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.15.0
@@ -19,7 +20,7 @@ require (
 	github.com/docker/distribution v2.8.1+incompatible // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
+	github.com/moby/term v0.0.0-20221205130635-1aeaba878587
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
@@ -31,6 +32,7 @@ require (
 )
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
@@ -40,7 +42,6 @@ require (
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
@@ -54,6 +55,5 @@ require (
 	golang.org/x/text v0.6.0 // indirect
 	golang.org/x/tools v0.5.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/docker/image.go
+++ pkg/docker/image.go
@@ -2,26 +2,14 @@ package docker
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
+	"os"
 	"strings"
 
-	"github.com/ChrisWiegman/kana-cli/pkg/console"
-
 	"github.com/docker/docker/api/types"
+	"github.com/moby/moby/pkg/jsonmessage"
+	"github.com/moby/term"
 )
-
-type pullEvent struct {
-	ID             string `json:"id"`
-	Status         string `json:"status"`
-	Error          string `json:"error,omitempty"`
-	Progress       string `json:"progress,omitempty"`
-	ProgressDetail struct {
-		Current int `json:"current"`
-		Total   int `json:"total"`
-	} `json:"progressDetail"`
-}
 
 // https://gist.github.com/miguelmota/4980b18d750fb3b1eb571c3e207b1b92
 // https://riptutorial.com/docker/example/31980/image-pulling-with-progress-bars--written-in-go
@@ -30,18 +18,15 @@ func (d *DockerClient) EnsureImage(imageName string) (err error) {
 		imageName = fmt.Sprintf("%s:latest", imageName)
 	}
 
-	events, err := d.client.ImagePull(context.Background(), imageName, types.ImagePullOptions{})
+	reader, err := d.client.ImagePull(context.Background(), imageName, types.ImagePullOptions{})
 	if err != nil {
 		return err
 	}
 
-	defer events.Close()
+	defer reader.Close()
 
-	decoder := json.NewDecoder(events)
-
-	err = displayEventStatus(imageName, decoder)
-
-	return err
+	termFd, isTerm := term.GetFdInfo(os.Stderr)
+	return jsonmessage.DisplayJSONMessagesStream(reader, os.Stderr, termFd, isTerm, nil)
 }
 
 func (d *DockerClient) RemoveImage(image string) (removed bool, err error) {
@@ -58,82 +43,4 @@ func (d *DockerClient) RemoveImage(image string) (removed bool, err error) {
 	}
 
 	return false, nil
-}
-
-func displayEventStatus(imageName string, decoder *json.Decoder) error {
-	cursor := console.Cursor{}
-	layers := make([]string, 0)
-	oldIndex := len(layers)
-
-	var event *pullEvent
-
-	cursor.Hide()
-
-	downloading := ""
-
-	for {
-		err := decoder.Decode(&event)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-
-			return err
-		}
-
-		imageID := event.ID
-
-		// Check if the line is one of the final two ones
-		if event.Status != "Downloading" && event.Status != "Extracting" {
-			continue
-		}
-
-		// Check if ID has already passed once
-		index := 0
-		for i, v := range layers {
-			if v == imageID {
-				index = i + 1
-				break
-			}
-		}
-
-		// Move the cursor
-		if index > 0 {
-			diff := index - oldIndex
-
-			if diff > 1 {
-				down := diff - 1
-				cursor.MoveDown(down)
-			} else if diff < 1 {
-				up := diff*(-1) + 1
-				cursor.MoveUp(up)
-			}
-
-			oldIndex = index
-		} else {
-			layers = append(layers, event.ID)
-			diff := len(layers) - oldIndex
-
-			if diff > 1 {
-				cursor.MoveDown(diff) // Return to the last row
-			}
-
-			oldIndex = len(layers)
-		}
-
-		cursor.ClearLine()
-
-		if event.Status == "Downloading" || event.Status == "Extracting" {
-			if downloading != imageName {
-				fmt.Printf("Downloading latest docker image: %s\n", imageName)
-				downloading = imageName
-			}
-
-			fmt.Printf("%s: %s %s\n", event.ID, event.Status, event.Progress)
-		}
-	}
-
-	cursor.Show()
-
-	return nil
 }
