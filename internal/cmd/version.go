a1cd49b4aeeb5e23f95df6b7864f36dcb2f6c551
Chris Wiegman <chris@chriswiegman.com>
2023-01-21 19:55:29 -0500

    Fully convert Version output to JSON where needed

--- internal/cmd/version.go
+++ internal/cmd/version.go
@@ -1,19 +1,37 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/ChrisWiegman/kana-cli/pkg/console"
 	"github.com/spf13/cobra"
 )
 
 var Version, Timestamp string
 
+type VersionInfo struct {
+	Version, Timestamp string
+}
+
 func newVersionCommand(consoleOutput *console.Console) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "Displays version information for the Kana CLI.",
 		Run: func(cmd *cobra.Command, args []string) {
-			consoleOutput.Printf("Version: %s\n", Version)
-			consoleOutput.Printf("Build Time: %s\n", Timestamp)
+			if consoleOutput.JSON {
+				v := VersionInfo{
+					Version:   Version,
+					Timestamp: Timestamp,
+				}
+
+				str, _ := json.Marshal(v)
+
+				fmt.Println(string(str))
+			} else {
+				consoleOutput.Printf("Version: %s\n", Version)
+				consoleOutput.Printf("Build Time: %s\n", Timestamp)
+			}
 		},
 		Args: cobra.NoArgs,
 	}
