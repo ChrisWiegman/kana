0a3073215db236a5ab73841c5787a82bcaeca115
Chris Wiegman <chris@chriswiegman.com>
2023-02-04 19:36:56 -0500

    Clear line limit lint in mock

--- pkg/docker/mocks/jsonmessage.go
+++ pkg/docker/mocks/jsonmessage.go
@@ -8,6 +8,6 @@ import (
 
 var MockedDisplayJSONMessagesStreamReturn error
 
-func MockDisplayJSONMessagesStream(in io.Reader, out io.Writer, terminalFd uintptr, isTerminal bool, auxCallback func(jsonmessage.JSONMessage)) error {
+func MockDisplayJSONMessagesStream(in io.Reader, out io.Writer, terminalFd uintptr, isTerminal bool, auxCallback func(jsonmessage.JSONMessage)) error { //nolint:lll
 	return MockedDisplayJSONMessagesStreamReturn
 }
