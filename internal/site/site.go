cb73c04fbe68258353da09ad4d569f6c6e20925c
Chris Wiegman <chris@chriswiegman.com>
2023-06-18 11:45:09 -0400

    Move Xdebug functions to their own file for easier management

--- internal/site/site.go
+++ internal/site/site.go
@@ -304,62 +304,6 @@ func (s *Site) StopSite() error {
 	return s.maybeStopTraefik()
 }
 
-// StartXdebug installs and starts xdebug in the site's PHP container
-func (s *Site) StartXdebug(consoleOutput *console.Console) error {
-	commands := []string{
-		"pecl list | grep xdebug",
-		"pecl install xdebug",
-		"docker-php-ext-enable xdebug",
-		"echo 'xdebug.start_with_request=yes' >> /usr/local/etc/php/php.ini",
-		"echo 'xdebug.mode=debug,develop,trace' >> /usr/local/etc/php/php.ini",
-		"echo 'xdebug.client_host=host.docker.internal' >> /usr/local/etc/php/php.ini",
-		"echo 'xdebug.discover_client_host=on' >> /usr/local/etc/php/php.ini",
-		"echo 'xdebug.start_with_request=trigger' >> /usr/local/etc/php/php.ini",
-		"echo 'xdebug.show_local_vars=1' >> /usr/local/etc/php/php.ini",
-		"echo 'html_errors = On' >> /usr/local/etc/php/conf.d/z-custom.ini", // Ensure custom overrides happen
-	}
-
-	for i, command := range commands {
-		restart := false
-
-		if i+1 == len(commands) {
-			restart = true
-		}
-
-		output, err := s.runCli(command, restart)
-		if err != nil {
-			return err
-		}
-
-		// Verify that the command ran correctly
-		if i == 0 && strings.Contains(output.StdOut, "xdebug") {
-			return nil
-		}
-	}
-
-	return nil
-}
-
-// StopXdebug stops Xdebug by restarting the WordPress containers
-func (s *Site) StopXdebug(consoleOutput *console.Console) error {
-	err := s.stopWordPress()
-	if err != nil {
-		return err
-	}
-
-	return s.startWordPress(consoleOutput)
-}
-
-// XdebugStatus returns true if Xdebug is already running or false if not
-func (s *Site) XdebugStatus(consoleOutput *console.Console) bool {
-	output, err := s.runCli("pecl list | grep xdebug", false)
-	if err != nil {
-		return false
-	}
-
-	return strings.Contains(output.StdOut, "xdebug")
-}
-
 // checkStatusCode returns true on 200 or false
 func checkStatusCode(checkURL string) (bool, error) {
 	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, checkURL, http.NoBody)
