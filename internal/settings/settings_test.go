973f3bdc3ebf143389884b9e9e8bf7e01756e708
Chris Wiegman <chris@chriswiegman.com>
2023-06-18 12:01:09 -0400

    Remove erroneous print statement

--- internal/settings/settings_test.go
+++ internal/settings/settings_test.go
@@ -1,7 +1,6 @@
 package settings
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,8 +8,6 @@ import (
 
 func TestNewSettings(t *testing.T) {
 	settings, err := NewSettings()
-
-	fmt.Println(settings.Activate)
 
 	assert.False(t, settings.Activate)
 	assert.Equal(t, nil, err)
