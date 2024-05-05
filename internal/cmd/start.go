2f405cd952e4d71fbfa9cffee5959efdfafe61ef
Chris Wiegman <chris@chriswiegman.com>
2024-05-05 15:56:18 -0500

    Ensure type detection works

--- internal/cmd/start.go
+++ internal/cmd/start.go
@@ -123,7 +123,7 @@ func handleTypeDetection(cmd *cobra.Command, consoleOutput *console.Console, kan
 
 		if siteType != kanaSite.Settings.Type {
 			kanaSite.Settings.Type = siteType
-			return fmt.Errorf("a %s was detected as the current site folder. Starting site as a %s", siteType, siteType)
+			consoleOutput.Printf("A %s was detected in the current site folder. Starting site as a %s", siteType, siteType)
 		}
 	}
 
