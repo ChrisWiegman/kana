de6b7ffdf6ed4dfc3f8f66cb4a0eff5ed80d0be0
Chris Wiegman <chris@chriswiegman.com>
2024-06-05 14:28:17 -0500

    Increase assertions in tests

--- internal/settings/settings_test.go
+++ internal/settings/settings_test.go
@@ -9,6 +9,7 @@ import (
 func TestNewSettings(t *testing.T) {
 	settings, err := NewSettings("1.0.0")
 
+	assert.NotNil(t, settings)
 	assert.False(t, settings.Activate)
 	assert.Equal(t, nil, err)
 }
