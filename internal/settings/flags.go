647aed25b836cc5e46448f4b5de4990046d2d177
Chris Wiegman <chris@chriswiegman.com>
2024-06-27 11:27:08 -0500

    Refactor for easier retrieval

--- internal/settings/flags.go
+++ internal/settings/flags.go
@@ -10,44 +10,24 @@ import (
 func processStartFlags(cmd *cobra.Command, flags *StartFlags, settings *Settings) {
 	options := settings.settings
 
-	if cmd.Flags().Lookup("xdebug").Changed {
-		options.Xdebug = flags.Xdebug
+	if cmd.Flags().Lookup("activate").Changed {
+		options.Activate = flags.Activate
 	}
 
-	if cmd.Flags().Lookup("wpdebug").Changed {
-		options.WPDebug = flags.WPDebug
+	if cmd.Flags().Lookup("database").Changed {
+		options.Database = flags.Database
 	}
 
-	if cmd.Flags().Lookup("scriptdebug").Changed {
-		options.ScriptDebug = flags.ScriptDebug
-	}
-
-	if cmd.Flags().Lookup("ssl").Changed {
-		options.SSL = flags.SSL
+	if cmd.Flags().Lookup("environment").Changed {
+		options.Environment = flags.Environment
 	}
 
 	if cmd.Flags().Lookup("mailpit").Changed {
 		options.Mailpit = flags.Mailpit
 	}
 
-	if cmd.Flags().Lookup("type").Changed {
-		options.Type = flags.Type
-	}
-
-	if cmd.Flags().Lookup("theme").Changed {
-		options.Theme = flags.Theme
-	}
-
 	if cmd.Flags().Lookup("multisite").Changed {
 		options.Multisite = flags.Multisite
-	}
-
-	if cmd.Flags().Lookup("activate").Changed {
-		options.Activate = flags.Activate
-	}
-
-	if cmd.Flags().Lookup("environment").Changed {
-		options.Environment = flags.Environment
 	}
 
 	if cmd.Flags().Lookup("plugins").Changed {
@@ -58,7 +38,27 @@ func processStartFlags(cmd *cobra.Command, flags *StartFlags, settings *Settings
 		options.RemoveDefaultPlugins = flags.RemoveDefaultPlugins
 	}
 
-	if cmd.Flags().Lookup("database").Changed {
-		options.Database = flags.Database
+	if cmd.Flags().Lookup("scriptdebug").Changed {
+		options.ScriptDebug = flags.ScriptDebug
+	}
+
+	if cmd.Flags().Lookup("ssl").Changed {
+		options.SSL = flags.SSL
+	}
+
+	if cmd.Flags().Lookup("theme").Changed {
+		options.Theme = flags.Theme
+	}
+
+	if cmd.Flags().Lookup("type").Changed {
+		options.Type = flags.Type
+	}
+
+	if cmd.Flags().Lookup("wpdebug").Changed {
+		options.WPDebug = flags.WPDebug
+	}
+
+	if cmd.Flags().Lookup("xdebug").Changed {
+		options.Xdebug = flags.Xdebug
 	}
 }
