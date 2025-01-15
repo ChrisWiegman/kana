83fad3e136265d50f26541d9972593f0e7b03c13
Chris Wiegman <chris@chriswiegman.com>
2025-01-14 18:17:20 -0600

    Change default PHP version to 8.3

--- internal/settings/defaults.go
+++ internal/settings/defaults.go
@@ -151,7 +151,7 @@ var defaults = []Setting{
 	},
 	{
 		name:         "php",
-		defaultValue: "8.2",
+		defaultValue: "8.3",
 		settingType:  "string",
 		hasLocal:     true,
 		hasGlobal:    true,
